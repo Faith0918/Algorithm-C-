83b9087870c26639cd7658bb310b23b0ac6bb00a
Faith0918 <sinsil0918@gmail.com>
2020-06-03 11:57:08 +0900

    day17

--- alpha-yut-d.cpp
+++ alpha-yut-d.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int n;
 int result = 0;
-int map[31] = { 0,5,10,15,20,50,30,35,40,45,100,55,60,65,70,75,80,85,90,95,500,1000,275,250,300,150,175,150,125,350,400 };
+int map[35] = { 0,5,10,15,20,50,30,35,40,45,100,55,60,65,70,75,80,85,90,95,500,1000,275,250,300,150,175,150,125,350,400 };
 int score[11] = {};
 int horse[5] = { 0 };
-int m[31][5] = { 0 };
+int m[35][6] = { 0 };
 int main() {
 
     int temp[11][6] = { {5,22,23,24,25,26},
@@ -44,7 +44,6 @@ int main() {
         for (int j = 0; j < 11; j++) {
             if (temp[j][0] == horse[seq[i]]) {
                 flag = true;
-                return 0;
             }
         }
         if (flag) {
@@ -62,7 +61,7 @@ int main() {
         }
         if (!finish) {
             for (int j = 1; j < 5; j++) {
-                if (j != seq[i] && horse[seq[i]] == horse[seq[j]]) {
+                if (j != seq[i] && horse[seq[i]] == horse[j]) {
                     //crash
                     cout << "-1\n";
                     return 0;
@@ -72,7 +71,7 @@ int main() {
 
     }
     for (int i = 1; i < 5; i++) {
-        result += a[horse[i]];
+        result += map[horse[i]];
     }
     cout << result;
 
