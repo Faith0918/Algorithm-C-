60e30b2d1de27ca6bc972f2f573ea19f2c97c952
Faith0918 <53809581+Faith0918@users.noreply.github.com>
2020-10-22 08:15:21 +0900

    Update kick_start.cpp

--- kick_start.cpp
+++ kick_start.cpp
@@ -9,18 +9,12 @@ int solve(string s) {
     vector<int> start;
     int ans = 0;
     for (int i = 0; i < s.size(); i++) {
-        cout << s[i] << "\n";
         if (i < s.size() - 2 && s.substr(i, 4) == "KICK") {
-            cout << s.substr(i, 4) << "\n";
             kick.push_back(i);
-            i += 3;
             continue;
         }
         else if (i < s.size() - 3 && s.substr(i, 5) == "START") {
-            cout << s.substr(i, 5) << "\n";
-            start.push_back(i);
             ans += kick.size();
-            i += 4;
             continue;
         }
     }
