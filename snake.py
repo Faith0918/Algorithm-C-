e3454203f027ff6e636a08d20c7155eed412614e
Faith0918 <sinsil0918@gmail.com>
2020-12-12 21:53:29 +0900

    snake_finished

--- snake.py
+++ snake.py
@@ -15,33 +15,39 @@ class Snake:
         self.length = 1
         self.direction = 0
     def move(self):
-        if (self.move_head()):
+        if self.move_head():
+            #print("head moved to "+str(self.q[0][0])+" "+str(self.q[0][1]))
             if (check_apple(self.q[0])):
                 # If there are apple
+                arr[self.q[0][0]][self.q[0][1]] = -1
                 self.length+=1
-                arr[self.q[0][0]][self.q[0][1]] = 0
+                #arr[self.q[0][0]][self.q[0][1]] = 0
             else:
+                arr[self.q[0][0]][self.q[0][1]] = -1
+                arr[self.q[len(self.q)-1][0]][self.q[len(self.q)-1][1]] = 0
                 self.q.pop()
+                
             return True
         else:
             return False
     def move_head(self):
         candi = (self.q[0][0]+dr[self.direction],self.q[0][1]+dc[self.direction])
         if (isPosssible(candi, self.q)):
-            self.q.appendleft((self.q[0][0]+dr[self.direction],self.q[0][1]+dc[self.direction]))
+            self.q.appendleft(candi)
             return True
         else:
             return False
         
 def isPosssible(candi, q):
     if candi[0]>0 and candi[1] > 0 and candi[0]<=N and candi[1] <=N:
-        for i in range(1,len(q)):
-            if candi[0] == q[i][0] and candi[1] == q[i][1]:
-                return False
-        return True
+        if arr[candi[0]][candi[1]] != -1:
+            return True
     else:
         return False
+
+    return False
 def check_apple(x):
+    #print("check : " + str(x))
     if arr[x[0]][x[1]] == 1:
         return True
     else:
@@ -54,9 +60,12 @@ def solve():
     # Initialize time
     answer = 0
     for t, d in time.items():
-        for j in range(t):
+        #print("t : "+str(t))
+        while answer < t:
+            answer+=1
+            # for i in range(len(arr)):
+            #     print(arr[i])
             if (snake.move()):
-                answer+=1
                 continue
             else:
                 return answer
@@ -64,6 +73,13 @@ def solve():
             snake.direction = (snake.direction + 1)%4
         else:
             snake.direction = (snake.direction - 1)%4
+    while True:
+        answer +=1
+        if(snake.move()):
+            continue
+        else:
+            return answer
+    
 
 
 
@@ -72,6 +88,7 @@ N = int(input())
 K = int(input())
 # Initialize map
 arr = [[0]*(N+1) for _ in range(N+1)]
+arr[1][1] = -1
 for i in range(K):
     a,b = map(int, input().split())
     arr[a][b] = 1
