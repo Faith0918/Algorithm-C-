270468e328091e09cef570f7435e9aadf59d8cab
Faith0918 <sinsil0918@gmail.com>
2020-12-14 11:26:40 +0900

    hammock_finish

--- hammock.py
+++ hammock.py
@@ -1,7 +1,10 @@
+# 오답노트 : 같은 x, y 에 기둥과 보가 오는 경우가 있어서 이렇게 하면 안된다.
+
 def Execute(x,y,a,b,arr):
     if b ==1 : # 설치
         if a == 0 : # 기둥
-            if y == 0 or arr[y][x-1] == 1 or arr[y-1][x] == 0: #바닥/ 보의 한쪽 끝/ 기둥 위에 있으면 설치
+            if y == 1 or arr[y][x-1] == 1 or arr[y-1][x] == 0: #바닥/ 보의 한쪽 끝/ 기둥 위에 있으면 설치
+                
                 arr[y][x] = 0
         else: #보
             if arr[y-1][x] == 0 or (arr[y][x-1] ==1 and arr[y][x+1] ==1) or arr[y-1][x+1]==0:
@@ -17,14 +20,19 @@ def Execute(x,y,a,b,arr):
 
 def solution(n, build_frame):
     answer = []
-    print("SS")
-    arr = [[-1]*6 for _ in range(1001)]
+    arr = [[-1]*(n+3) for _ in range(n+3)]
     for x, y, a, b in build_frame:
-        Execute(x,y,a,b,arr)
+        if x >=0 and x <=n and y >=0 and y <=n:
+            Execute(x+1,y+1,a,b,arr)
     
-    for c in range(6):
-        for r in range(1001):
+    for c in range(n+2):
+        for r in range(n+2):
             if arr[r][c] >=0:
-                answer.append([c,r,arr[r][c]])
+                answer.append([c-1,r-1,arr[r][c]])
     print(answer)
     return answer
+
+if __name__ == "__main__":
+    n = 2
+    build_frame = [[0,0,0,1],[1,0,0,1],[0,1,1,1], [0,1,0,1]]
+    solution(n, build_frame)
