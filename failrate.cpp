fca7b9c654b1bcef1747094409a488e0ed3c7031
Faith0918 <sinsil0918@gmail.com>
2021-02-04 06:57:35 +0900

    failrate-finished

--- failrate.cpp
+++ failrate.cpp
@@ -1,29 +1,45 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+bool cmp(pair<int, float> &a, pair<int,float> &b) {
+    if (a.second > b.second) {
+        return true;
+    }
+    else if(a.second == b.second){
+        return a.first < b.first;
+    }
+    else {
+        return false;
+    }
+}
+
+
+
 vector<int> solution(int N, vector<int> stages) {
     vector<int> answer;
     vector<pair<int, float>> v;
-
-    for (int i = 0; i < N; i++) {
-        answer.push_back(0);
-    }
+    
+    sort(stages.begin(), stages.end());
     int before = stages[0];
     int cnt = 0;
-    sort(stages.begin(), stages.end());
+    for (int i = 0; i < stages.size(); i++) {
+        cout<<stages[i]<<"\t";
+    }
     for (int i = 0; i < stages.size(); i++) {
         if (stages[i] > before) {
             cout << "stage : " << before << "\n";
-            if ((N + cnt - i) == 0) {
+            if ((stages.size() + cnt - i) == 0) {//끝
                 v.push_back(make_pair(N, 0.0));
                 cnt = 1;
                 before = stages[i];
             }
             else {
-                float fail = float(cnt) / float(N + cnt - i);
+                float fail = float(cnt) / float(stages.size() + cnt - i);
+                cout << "cnt : " << cnt << "\n";
+                cout << "개수 : " << stages.size() + cnt - i<<"\n";
                 cout << "fail : " << fail << "\n";
                 v.push_back(make_pair(before, fail));
                 cnt = 1;
@@ -32,14 +48,31 @@ vector<int> solution(int N, vector<int> stages) {
             
         }
         else {
+            cout << "what? : " << stages[i]<<"\n";
+            cout << "before : " << before << "\n";
+            cout << "cnt : "<<cnt<< "\n";
             cnt++;
         }
     }
     if (cnt > 0 && before<N+1) {
         v.push_back(make_pair(before, 1.0));
     }
+    vector<bool> check(N + 1);
     for (int i = 0; i < v.size(); i++) {
         cout << v[i].first << " " << to_string(v[i].second) << "\n";
+    }
+    for (int i = 1; i < N+1; i++) {
+        if (find_if(v.begin(), v.end(), [&i](const pair<int, float>& p) {return p.first == i; }) == v.end()) {
+            v.push_back(make_pair(i, 0.0));
+        }
+        
+    }
+    sort(v.begin(), v.end(), cmp);
+    for (int i = 0; i < N; i++) {
+        answer.push_back(v[i].first);
+    }
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << "\t";
     }
     return answer;
 }
@@ -55,7 +88,8 @@ int main() {
     stages.push_back(3);
     stages.push_back(3);
     vector<int> ans =  solution(N, stages);
+    cout << "\n\n";
     for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i]<<"\t";
+        cout << ans[i] << "\t";
     }
 }
