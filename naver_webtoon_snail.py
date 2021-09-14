92a7954d175da8f3edca74c925f3faac83f322ae
Faith0918 <53809581+Faith0918@users.noreply.github.com>
2021-09-14 15:25:09 +0900

    Update naver_webtoon_snail.py

--- naver_webtoon_snail.py
+++ naver_webtoon_snail.py
@@ -1,6 +1,6 @@
 import pdb
 from collections import deque
-a = [2,3]
+a = [4,2]
 total_l = 1
 for x in a:
     total_l *= x
@@ -16,9 +16,9 @@ def paint(start, leng):
     s_r, s_c = start
     q = deque()
     q.append(start)
-    print("start:",start, leng)
     board[s_r][s_c] = count
     count+=1
+    flag = False
     while True:
         if len(q) ==0:
             break
@@ -27,26 +27,30 @@ def paint(start, leng):
         n_c = c_c + dc[d]
         if s_r <= n_r < s_r + leng and s_c <= n_c < s_c + leng:
             if board[n_r][n_c] == 0:
+                flag = False
                 board[n_r][n_c] = count
                 count +=1
                 q.append([n_r,n_c])
             else:
-                break
+                if flag:
+                    break
+                else:
+                    d = (d+1)%4
+                    q.append([c_r,c_c])
+                    flag = True
         else:
             d = (d+1)%4
             q.append([c_r,c_c])
-    for r in range(total_l):
-        print(board[r])
 
 
 def dfs(cnt, start, leng):
     if cnt == 0:
-        print("paint")
+        # print("paint")
         paint(start, leng)
     else:
         c_r, c_c = start
         n_l = leng // a[cnt]
-        print("n_l", a[cnt], n_l, a[cnt])
+        # print("n_l", a[cnt], n_l, a[cnt])
         visited = [[False for _ in range(a[cnt])] for _ in range(a[cnt])]
         # print(visited)
         q = deque()
@@ -54,10 +58,11 @@ def dfs(cnt, start, leng):
         d = 0
         dfs(cnt-1, start, n_l)
         visited[0][0] = True
+        flag = False
         while q:
             
             c_r, c_c = q.popleft()
-            print(visited, d)
+            # print(visited, d)
             # print("cur",c_r, c_c)
             n_r = c_r + dr[d]
             n_c = c_c + dc[d]
@@ -65,13 +70,17 @@ def dfs(cnt, start, leng):
             # pdb.set_trace()
             if 0 <= n_r < a[cnt] and 0<= n_c < a[cnt]:
                 if visited[n_r][n_c] == False:
+                    flag= False
                     visited[n_r][n_c] = True
                     q.append([n_r, n_c])
                     dfs(cnt-1, [start[0] +n_r*n_l, start[1] + n_c*n_l], n_l)
                 else:
-                    # print("else")
-                    # print(visited)
-                    break
+                    if flag:
+                        break
+                    else:
+                        d = (d+1)%4
+                        q.append([c_r,c_c])
+                        flag = True
             else:
                 d = (d+1)%4
                 q.append([c_r,c_c])
